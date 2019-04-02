136befec7d7838a05bdb402b7d69414047d15c61
premruethai <pramrutai34@gmail.com>
2019-04-03 00:03:35 +0700

    การบ้าน

--- src/pages/result/result.ts
+++ src/pages/result/result.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 
 /**
  * Generated class for the ResultPage page.
@@ -8,14 +9,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
 @IonicPage()
 @Component({
   selector: 'page-result',
   templateUrl: 'result.html',
 })
 export class ResultPage {
+  cus:any=0;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    let cusid=this.navParams.get('keyword');
+    let url="http://localhost:8080/customer/name/"+cusid;
+    console.log(url);
+    this.http.get(url)
+    .map(res=>res.json())
+    .subscribe(data=>{
+      this.cus=data;
+    });
+
+  }
+
+  showDetail(id){
+    this.navCtrl.push(DetailPage,{ID_customer : id});
   }
 
   ionViewDidLoad() {
